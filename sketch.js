2a5aa549444121d88135133f825381578200fcc8
AJITESH3210 <jalluri3210@gmail.com>
2020-06-27 17:14:22 +0530

    Files added

--- sketch.js
+++ sketch.js
@@ -9,18 +9,17 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-var bg = "sprites/bg1.png";
+var background;
 var score = 0;
 
 function preload() {
-    getBackgroundImg();
+    backgroundImg = "sprites/sscchhooooll.png";
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -47,8 +46,9 @@ function setup(){
 }
 
 function draw(){
-    if(backgroundImg)
-        background(backgroundImg);
+
+ 
+    background("sprites/sscchhooooll.png");
     
         noStroke();
         textSize(35)
@@ -98,22 +98,4 @@ function keyPressed(){
        Matter.Body.setPosition(bird.body, {x : 200, y : 50});
        slingshot.attach(bird.body);
     }
-}
-
-async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-    var responseJSON = await response.json();
-
-    var datetime = responseJSON.datetime;
-    var hour = datetime.slice(11,13);
-    
-    if(hour>=0600 && hour<=1900){
-        bg = "sprites/bg1.png";
-    }
-    else{
-        bg = "sprites/bg2.jpg";
-    }
-
-    backgroundImg = loadImage(bg);
-    console.log(backgroundImg);
 }
